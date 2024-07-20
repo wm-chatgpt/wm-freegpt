d5d3e13b03d673421369550756a684d89c048d12
lihang15 <lihang15@meituan.com>
2024-07-21 01:11:41 +0800

    init

--- main.go
+++ main.go
@@ -12,13 +12,9 @@ import (
 
 func main() {
 	ctx := gctx.New()
-	// 加载arkose模块
 	arkose.Init(ctx)
-	// 加载api模块
 	api.Init(ctx)
-	// 加载next模块
 	next.Init(ctx)
-	// 加载backend-api模块
 	backendapi.Init(ctx)
 
 	// 启动HTTP服务
