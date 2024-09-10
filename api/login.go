d40453ae50475b95853b106fbd155bd310917dcd
lihang15 <lihang15@meituan.com>
2024-09-10 18:50:30 +0800

    remix

--- api/login.go
+++ api/login.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"cockroachai/config"
+	"fmt"
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"strings"
 )
 
 func Login(r *ghttp.Request) {
@@ -14,6 +16,12 @@ func Login(r *ghttp.Request) {
 	}
 	r.Response.WriteTpl("login.html", g.Map{})
 }
+
+var errHtml = `
+<span id="error-element-password" class="ulp-input-error-message"
+                                              data-error-code="wrong-email-credentials">
+                        <span class="ulp-input-error-icon" role="img" aria-label="错误"></span>
+                        %s</span>`
 
 func LoginPost(r *ghttp.Request) {
 	ctx := r.GetCtx()
@@ -25,7 +33,12 @@ func LoginPost(r *ghttp.Request) {
 	sessionJson := gjson.New(sessionVar)
 	if sessionJson.Get("accessToken").String() == "" {
 		g.Log().Info(ctx, "登录失败")
-		r.Response.RedirectTo("/login")
+		var errc = "登录失败请重试"
+		if strings.Contains(sessionJson.Get("detail").String(), "Wrongemailorpassword") {
+			errc = "账号密码错误"
+		}
+		r.Response.WriteTpl("login.html", g.Map{"errInfo": fmt.Sprintf(errHtml, errc)})
+		return
 	}
 	r.Cookie.Set(config.AccessToken, sessionJson.Get("accessToken").String())
 	r.Cookie.Set(config.RefreshCookie, sessionJson.Get("refreshCookie").String())
